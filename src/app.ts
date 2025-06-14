db9319592de827dbff4466185e7dbdce29e3c562
Ashish Verma <ashuverma.stp@gmail.com>
2025-06-14 23:18:12 +0530

    add swagger documentation (#2)

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import emailRoutes from "./routes/email.routes";
+
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./configs/swagger";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 app.use("/api", emailRoutes);
 
+// Swagger UI route
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 export default app;
